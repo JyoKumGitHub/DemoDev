002882a59c4f905cfc41e82e708576185393814a
A07076_h4.02.13 <A07076_h4.02.13@accenture.com>
2018-09-04 11:45:19 +0530

    adding demoJenkins notepad java file

---
+++ DemoJenkins.java
@@ -0,0 +1,21 @@
+class DemoJenkins
+{  
+   public static void main (String args[])
+   {
+     System.out.println("********** 1 Hello JENKINS 1 ********");
+     System.out.println("********** 2 HOST : JAVA 2 ********");
+     System.out.println("********** 3 GUEST : JENKINS 3 ********");
+
+      System.out.println("---Call myMethod Now ---");
+      myMethod();
+      System.out.println("---Call myMethod Successfull ---");
+  }
+
+     public static void myMethod(){
+    
+       System.out.println("*** Call myMethod ***");
+
+    }
+  
+}
+
